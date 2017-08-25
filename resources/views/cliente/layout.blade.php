f969013bbb0867ce1bd094271328561bee34da6e
lubarinobr <matheus.lubarino1@gmail.com>
2017-08-25 01:42:41 -0300

    adjust resource page

--- resources/views/cliente/layout.blade.php
+++ resources/views/cliente/layout.blade.php
@@ -88,9 +88,9 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <script src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
 <script src="https://cdn.jsdelivr.net/bootstrap.daterangepicker/2/daterangepicker.js"></script>
-<script type="text/javascript" src="{{ public('js/controlsConfig.js') }}"></script>
-<script type="text/javascript" src="{{ public('js/jquery.mask.min.js') }}"></script>
-<script type="text/javascript" src="{{ public('js/jquery.mask.usage.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js/controlsConfig.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js/jquery.mask.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js/jquery.mask.usage.js') }}"></script>
 
 <script>
   window.Laravel = <?php echo json_encode([
